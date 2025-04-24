bfc023ab72c787b8613091f4cdfc5952e28a2b05
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2025-04-24 11:09:43 -0400

    removed data table, added sclass descriptions table

---
+++ styles.scss.R
@@ -0,0 +1,15 @@
+
+
+
+/*-- scss:defaults --*/
+  $body-color: #ffffff; /* Change body text color to white */
+  $table-color: #ffffff; /* Change table text color to white */
+  
+  /*-- scss:rules --*/
+  body {
+    color: $body-color;
+  }
+
+table {
+  color: $table-color;
+}
